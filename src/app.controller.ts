d9e2ee348aa02d1f63585724568122027e300b3a
Karan Kaushal <karan.1123@zenmonk.tech>
2023-08-21 17:01:21 +0530

    Code committed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
-import { ReportType, data } from 'src/data';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ReportType, data } from './data';
+import { v4 as uuid } from "uuid";
 // Creating a new controller entity
 
 // Every single entity inside a nestjs is a class
@@ -15,11 +16,30 @@ export class AppController {
   }
 
   @Get(':id')
-  getIncomeReportById() {
-    return {};
+  getIncomeReportById(@Param('type') type: string, @Param('id') id: string) {
+    console.log({
+      type,
+      id
+    });
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType).find((report) => report.id === id);
   }
   @Post()
-  createIncomeReport() {
+  createIncomeReport(
+    @Body() body: {
+      amount: number,
+      source: string
+    },
+    @Param('type') type: string
+  ) {
+    // console.log({
+    //   body
+    // });
+    const newReport = {
+      id: uuid(),
+      source: body.source,
+      amount: body.amount,
+    }
     return 'Created';
   }
 
